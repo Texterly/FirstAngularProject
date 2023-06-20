ea1eae7fd7899f2355ea90d87c379146c0bca742
Texterly <88582978+Texterly@users.noreply.github.com>
2023-06-20 11:54:15 +0300

    add create product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {ProductComponent} from "./components/product/product.component";
+import {GlobalErrorComponent} from "./components/global-error/global-error.component";
 import {HttpClientModule} from "@angular/common/http";
-import { GlobalErrorComponent } from './components/global-error/global-error.component';
-import {FormsModule} from "@angular/forms";
-import { FilterProductsPipe } from './pipes/filter-products.pipe';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FilterProductsPipe} from "./pipes/filter-products.pipe";
+import {ModalComponent} from "./components/modal/modal.component";
+import {CreateProductComponent} from "./components/create-product/create-product.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     GlobalErrorComponent,
-    FilterProductsPipe
+    FilterProductsPipe,
+    ModalComponent,
+    CreateProductComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
